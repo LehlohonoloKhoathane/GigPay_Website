52e67aa7968f771f04b50cda6fca031d6a1533ea
LehlohonoloKhoathane <khoathanelehlohonolo@gmail.com>
2025-07-19 01:05:22 +0200

    footer and About

--- gigpay/src/App.jsx
+++ gigpay/src/App.jsx
@@ -5,16 +5,17 @@ import Header from './pages/header/header'
 import Home from './pages/home/home';
 import About from './pages/about/about';
 import Products from './pages/products/products';
+import Footer from './pages/footer/footer';
 
 function App() {
   
-
   return (
     <BrowserRouter>
     <Header/>
     <Home/>
     <About/>
     <Products/>
+    <Footer/>
     </BrowserRouter>
   )
 }
---
+++ gigpay/src/pages/footer/footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./footer.css";
+import googlePlay from '../../assets/googleplay.png'
+import appStore from '../../assets/istore.png'
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <h3 className="brand">GIGPAY</h3>
+
+        <div className="store-buttons">
+          <img src={googlePlay} alt="Google Play" />
+          <img src={appStore} alt="App Store" />
+        </div>
+
+        <div className="footer-links">
+          <span>Personal Loans</span>
+          <span>Savings</span>
+          <span>Mobile Wallet</span>
+          <span>Contacts</span>
+        </div>
+
+        <div className="footer-socials">
+          <FaFacebookF />
+          <FaInstagram />
+          <FaLinkedinIn />
+        </div>
+
+        <div className="footer-policies">
+          <span>Privacy Policy</span>
+          <span>Terms & Conditions</span>
+          <span>Copy Right Policy</span>
+          <span>Data Policy</span>
+        </div>
+
+        <div className="footer-bottom">
+          <p>@2025 GigPay | All rights reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
